995e97acb1ec7ade3427196c9b3ec2fc3bcfbb1f
santan47 <santankumarsharma47@gmail.com>
2017-05-30 00:04:08 +0530

    caculator

---
+++ calculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class calculator {
+	public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	Scanner scan1=new Scanner(System.in);
+	int x = scan.nextInt() , y = scan1.nextInt() , z;
+	z = x+y;
+	System.out.println("Answer =" + z);
+        }
+}
