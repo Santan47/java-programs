e8a3e8a891c2418483f0ac5b1cd746497bef8243
santan47 <santankumarsharma47@gmail.com>
2017-06-01 00:21:45 +0530

    read elements of array

--- array.java
+++ array.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class array{
 public static void main(String[] args)
 {
-int array[]={8,6,9,4,2};
+int[] array =new int[5];
+Scanner scan=new Scanner(System.in);
+for(int i=0;i<5;i++)
+{
+array[i]=scan.nextInt();
+}
 for(int element : array)
 {
 System.out.println(element);
