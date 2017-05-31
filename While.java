50dec737259407069316e715e49dda2ee9babc21
santan47 <santankumarsharma47@gmail.com>
2017-05-31 23:16:27 +0530

    while loop in java

---
+++ While.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class While{
+public static void main(String[] args){
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+while(a<50)
+{
+System.out.println(a);
+a++;
+}
+}
+}
